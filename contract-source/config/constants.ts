5b613a7e206e8433d6a45a61aab710e0fd913790
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-05-09 03:36:32 -0700

    chore: remove contract-source directory, moved to @primodiumxyz/contracts package

--- contract-source/config/constants.ts
+++
@@ -1,6 +0,0 @@
-import { EResource } from "./enums";
-
-export const DECIMALS = 18;
-export const SCALE = 10 ** DECIMALS;
-export const BASE_RESERVE = 1000;
-export const RESERVE_RESOURCE = EResource.Kimberlite;
