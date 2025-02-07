6f2539d2c32938cb369487aab4f6ef6153e1c9bf
Emerson Hsieh <emersonhsieh@berkeley.edu>
2025-02-07 08:32:24 -0800

    fix: next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
+const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-};
+  defaultShowCopyCode: true,
+});
 
-export default nextConfig;
+export default withNextra({});
